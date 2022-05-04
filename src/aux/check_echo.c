22a8ba61b267c3bce9328a10e0f671fd3107b65d
Letícia Marques <leticia.marquess@protonmail.com>
2022-05-04 17:00:01 -0300

    implemented echo

--- src/aux/check_echo.c
+++ src/aux/check_echo.c
@@ -6,7 +6,7 @@
 /*   By: lemarque <lemarque@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/22 20:14:19 by lemarque          #+#    #+#             */
-/*   Updated: 2022/04/29 21:22:45 by lemarque         ###   ########.fr       */
+/*   Updated: 2022/05/04 16:59:29 by lemarque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,10 @@
 int	check_echo(t_node **cmd)
 {
 	t_node	*aux;
+	int		i;
 
-	aux = (*cmd)->first_arg;
+	i = 0;
+	aux = (*cmd)->first_arg->next;
 	while (aux)
 	{
 		if (ft_strcmp(aux->val, "\'") == 0 || \
@@ -25,7 +27,15 @@ int	check_echo(t_node **cmd)
 			free(aux->val);
 			aux->val = ft_strdup("");
 		}
+		if (ft_strcmp(aux->val, "-n") == 0)
+		{
+			i = 1;
+			aux = aux->next;
+		}
+		printf("%s", aux->val);
 		aux = aux->next;
 	}
-	return (1);
+	if (i != 1)
+		printf("\n");
+	return (0);
 }
